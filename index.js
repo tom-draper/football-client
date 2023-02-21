c31b501d930ec1a4f3bf492ee4076127059c26b9
Tom Draper <tomjdraper1@gmail.com>
2023-02-21 20:36:49 +0000

    Display inplay games

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-import fetch from 'node-fetch';
-import * as dotenv from 'dotenv'
+import fetch from "node-fetch";
+import * as dotenv from "dotenv";
+import chalk from "chalk";
 
-dotenv.config()
+dotenv.config();
 
-const URL = 'https://api.football-data.org/v4/'
+const URL = "https://api.football-data.org/v4/";
 
-async function getData() {
-  console.log(process.env.X_AUTH_TOKEN)
-  const res = await fetch(URL + 'matches', {
+async function getData(endpoint) {
+  const res = await fetch(URL + endpoint, {
     headers: {
-      'X-Auth-Token': process.env.X_AUTH_TOKEN
-    }
+      "X-Auth-Token": process.env.X_AUTH_TOKEN,
+    },
   });
 
   if (res.status == 200) {
     return await res.json();
   } else {
-    return {'message': 'Error: Data fetch from API failed.'}
+    return { message: "Error: Data fetch from API failed." };
   }
 }
 
-let data = await getData();
+async function upcoming() {
+  const data = await getData("matches");
 
-console.log(data);
+  let inplay = [];
+  let scheduled = [];
+  for (let match of data.matches) {
+    if (match.status === "IN_PLAY" || match.status === "PAUSED") {
+      inplay.push(match);
+    } else {
+      scheduled.push(match);
+    }
+  }
+
+  console.log(chalk.blueBright("IN-PLAY:"));
+  for (let match of inplay) {
+    console.log(
+      `${chalk.grey(
+        new Date(match.utcDate).toLocaleTimeString("en-GB", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      )} ${match.homeTeam.shortName} ${chalk.yellowBright("0 - 0")} ${
+        match.awayTeam.shortName
+      } ${chalk.grey(`(${match.competition.name})`)}`
+    );
+  }
+  console.log();
+  console.log(chalk.blueBright("SCHEDULED:"));
+  for (let match of scheduled) {
+    console.log(
+      `${chalk.grey(
+        new Date(match.utcDate).toLocaleTimeString("en-GB", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      )} ${match.homeTeam.shortName} ${chalk.grey("vs")} ${
+        match.awayTeam.shortName
+      } ${chalk.grey(`(${match.competition.name})`)}`
+    );
+  }
+}
+
+upcoming();
